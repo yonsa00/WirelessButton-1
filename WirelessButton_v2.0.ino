68d1c842a21a9e46403c41de83d7140013fbe3d4
unknown <alfonsussandyh@gmail.com>
2023-06-02 20:41:19 +0700

    Add EEPROM Read for SN,Line,and Machine Name

--- WirelessButton_v2.0.ino
+++ WirelessButton_v2.0.ino
@@ -11,16 +11,25 @@ byte outc = 5;
 unsigned long cur_timeA, old_timeA, cur_timeB, old_timeB, cur_timeC, old_timeC;
 int stateA, stateB, stateC;
 int serialNumberAddress = 0, lineAddress = 25, machineNameAdddress = 50;
-string serialNumber, line, machineName;
+String serialNumber, line, machineName;
 
 void setup() {
   //begin serial and EEPROM
-  Serial.begin(115200);
+  Serial.begin(9600);
   EEPROM.begin(512);
+
   //read value from EEPROM
   serialNumber = read_EEPROM(serialNumberAddress);
   line = read_EEPROM(lineAddress);
   machineName = read_EEPROM(machineNameAdddress);
+  Serial.print("Serial Number :");
+  Serial.println(serialNumber);
+  Serial.print("Line :");
+  Serial.println(line);
+  Serial.print("Machine Name :");
+  Serial.println(machineName);
+  Serial.println("----------------------------------");
+
   //initialize IO
   pinMode(ina, INPUT);
   pinMode(inb, INPUT);
@@ -28,27 +37,28 @@ void setup() {
   pinMode(outa, OUTPUT);
   pinMode(outb, OUTPUT);
   pinMode(outc, OUTPUT);
+  delay(5000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (digitalRead(ina) == LOW) {
     digitalWrite(outa, LOW);
-    Serial.println("Lamp A Active");
+    Serial.print("Lamp A Active \t");
     stateA = 1;
   } else {
     digitalWrite(outa, HIGH);
-    Serial.println("Lamp A InActive");
+    Serial.print("Lamp A InActive \t");
     stateA = 0;
   }
 
   if (digitalRead(inb) == LOW) {
     digitalWrite(outb, HIGH);
-    Serial.println("Lamp B Active");
+    Serial.print("Lamp B Active \t");
     stateB = 1;
   } else {
     digitalWrite(outb, LOW);
-    Serial.println("Lamp B InActive");
+    Serial.print("Lamp B InActive \t");
     stateB = 0;
   }
 
@@ -61,5 +71,4 @@ void loop() {
     Serial.println("Lamp C InActive");
     stateC = 0;
   }
-  Send();
 }
