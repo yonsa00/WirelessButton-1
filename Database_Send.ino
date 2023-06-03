f86a8432c260d5a3f6f0dc28715a320eb5e802cf
unknown <alfonsussandyh@gmail.com>
2023-06-03 20:33:07 +0700

    add function for running LED Add function Send Database and Connect to WIfi Grouping address using struct

--- Database_Send.ino
+++ Database_Send.ino
@@ -1,3 +1,56 @@
-void database_Send(){
-  
+void _connectToWIFI() {
+  Serial.println(ssid);
+  Serial.println(password);
+  WiFi.begin(ssid, password);
+  int count = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    count += 1;
+    if (count >= 50) {
+      ESP.restart();
+    }
+  }
+  Serial.println("Wifi Connected Successfully");
+}
+
+String _generateUrl() {
+
+  String apiUrl = "http://";
+  apiUrl += String(host);
+  apiUrl += "/IGP/";
+  apiUrl += "WirelessButton";
+  apiUrl += ".php?";
+  apiUrl += "mode=update";
+  apiUrl += "&Line=" + line;
+  apiUrl += "&Status=" + String(curStateA) + String(curStateB) + String(curStateC);
+  apiUrl += "&Machine=S-1%20(NEW)" ;
+  Serial.println(apiUrl);
+  return apiUrl;
+}
+
+void _databaseSend() {
+  if (WiFi.status() != WL_CONNECTED) {
+    _connectToWIFI();
+  }
+  WiFiClient client;
+  HTTPClient http;
+  http.begin(client, _generateUrl());
+  int httpCode = http.GET();
+
+  if (httpCode > 0) {
+    // HTTP header has been send and Server response header has been handled
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    if (httpCode == HTTP_CODE_OK) {
+      const String& payload = http.getString();
+      Serial.println("received payload:\n<<");
+      Serial.println(payload);
+      Serial.println(">>");
+
+      oldStateA = curStateA;
+      oldStateB = curStateB;
+      oldStateC = curStateC;
+    }
+  }
+  http.end();
 }
--- WirelessButton_v2.0.ino
+++ WirelessButton_v2.0.ino
@@ -1,27 +1,46 @@
 #include <EEPROM.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
-byte ina = 12;
-byte inb = 13;
-byte inc = 14;
 
-byte outa = 2;
-byte outb = 4;
-byte outc = 5;
+struct EEPROMAddress {
+  int serialNumberAddress = 0;
+  int ssidAddress = 25;
+  int passwordAddress = 50;
+  int hostAddress = 75;
+  int portAddress = 100;
+  int lineAddress = 110;
+  int machineNameAddress = 135;
+};
+struct IOAddress {
+  byte ina = 12;
+  byte inb = 13;
+  byte inc = 14;
 
-unsigned long cur_timeA, old_timeA, cur_timeB, old_timeB, cur_timeC, old_timeC;
-int stateA, stateB, stateC;
-int serialNumberAddress = 0, lineAddress = 25, machineNameAdddress = 50;
+  byte outa = 2;
+  byte outb = 4;
+  byte outc = 5;
+};
+int curStateA, curStateB, curStateC, oldStateA, oldStateB, oldStateC;
+
 String serialNumber, line, machineName;
+String ssid, password, host, port;
 
 void setup() {
   //begin serial and EEPROM
   Serial.begin(9600);
   EEPROM.begin(512);
-
+  EEPROMAddress Address;
   //read value from EEPROM
-  serialNumber = read_EEPROM(serialNumberAddress);
-  line = read_EEPROM(lineAddress);
-  machineName = read_EEPROM(machineNameAdddress);
+  serialNumber = read_EEPROM(Address.serialNumberAddress);
+  Serial.println(read_EEPROM(Address.ssidAddress));
+  ssid = read_EEPROM(Address.ssidAddress);
+  password = read_EEPROM(Address.passwordAddress);
+  host = read_EEPROM(Address.hostAddress);
+  port = read_EEPROM(Address.portAddress);
+  line = read_EEPROM(Address.lineAddress);
+  machineName = read_EEPROM(Address.machineNameAddress);
+
   Serial.print("Serial Number :");
   Serial.println(serialNumber);
   Serial.print("Line :");
@@ -31,44 +50,54 @@ void setup() {
   Serial.println("----------------------------------");
 
   //initialize IO
-  pinMode(ina, INPUT);
-  pinMode(inb, INPUT);
-  pinMode(inc, INPUT);
-  pinMode(outa, OUTPUT);
-  pinMode(outb, OUTPUT);
-  pinMode(outc, OUTPUT);
-  delay(5000);
+  IOAddress ioAddress;
+  pinMode(ioAddress.ina, INPUT);
+  pinMode(ioAddress.inb, INPUT);
+  pinMode(ioAddress.inc, INPUT);
+  pinMode(ioAddress.outa, OUTPUT);
+  pinMode(ioAddress.outb, OUTPUT);
+  pinMode(ioAddress.outc, OUTPUT);
+
+  _connectToWIFI();
+  _checkLED();
+  delay(1000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (digitalRead(ina) == LOW) {
-    digitalWrite(outa, LOW);
+  IOAddress ioAddress;
+  if (digitalRead(ioAddress.ina) == LOW) {
+    digitalWrite(ioAddress.outa, LOW);
     Serial.print("Lamp A Active \t");
-    stateA = 1;
+    curStateA = 1;
   } else {
-    digitalWrite(outa, HIGH);
+    digitalWrite(ioAddress.outa, HIGH);
     Serial.print("Lamp A InActive \t");
-    stateA = 0;
+    curStateA = 0;
   }
 
-  if (digitalRead(inb) == LOW) {
-    digitalWrite(outb, HIGH);
+  if (digitalRead(ioAddress.inb) == LOW) {
+    digitalWrite(ioAddress.outb, HIGH);
     Serial.print("Lamp B Active \t");
-    stateB = 1;
+    curStateB = 1;
   } else {
-    digitalWrite(outb, LOW);
+    digitalWrite(ioAddress.outb, LOW);
     Serial.print("Lamp B InActive \t");
-    stateB = 0;
+    curStateB = 0;
   }
 
-  if (digitalRead(inc) == LOW) {
-    digitalWrite(outc, HIGH);
+  if (digitalRead(ioAddress.inc) == LOW) {
+    digitalWrite(ioAddress.outc, HIGH);
     Serial.println("Lamp C Active");
-    stateC = 1;
+    curStateC = 1;
   } else {
-    digitalWrite(outc, LOW);
+    digitalWrite(ioAddress.outc, LOW);
     Serial.println("Lamp C InActive");
-    stateC = 0;
+    curStateC = 0;
   }
+
+  if (curStateA != oldStateA || curStateB != oldStateB || curStateC != oldStateC) {
+    _databaseSend();
+  }
+
 }
